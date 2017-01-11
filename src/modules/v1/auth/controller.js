7f71604bc57f83d08641b4cd25493096cfe268f5
Nathan Loisel <nathan.loisel@apptitude.ch>
2017-01-11 15:51:02 +0100

    fix Authenticate handler documentation

--- src/modules/v1/auth/controller.js
+++ src/modules/v1/auth/controller.js
@@ -12,44 +12,58 @@ import passport from 'koa-passport'
  *     }
  */
 
-/**
- * @api {post} /v1/auth Authenticate user
- * @apiVersion 1.0.0
- * @apiName AuthUser
- * @apiGroup Auth
- *
- * @apiParam {String} username  User username.
- * @apiParam {String} password  User password.
- *
- * @apiExample Example usage:
- * curl -H "Content-Type: application/json" -X POST -d '{ "username": "johndoe@gmail.com", "password": "foo" }' localhost:5000/v1/auth
- *
- * @apiSuccess {Object}   user           User object
- * @apiSuccess {ObjectId} user._id       User id
- * @apiSuccess {String}   user.name      User name
- * @apiSuccess {String}   user.username  User username
- * @apiSuccess {String}   token          Encoded JWT
- *
- * @apiSuccessExample {json} Success-Response:
- *     HTTP/1.1 200 OK
- *     {
- *       "user": {
- *          "_id": "56bd1da600a526986cf65c80"
- *          "username": "johndoe"
- *        },
- *       "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWV9.TJVA95OrM7E2cBab30RMHrHDcEfxjoYZgeFONFh7HgQ"
- *     }
- *
- * @apiError Unauthorized Incorrect credentials
- *
- * @apiErrorExample {json} Error-Response:
- *     HTTP/1.1 401 Unauthorized
- *     {
- *       "status": 401,
- *       "error": "Unauthorized"
- *     }
- */
+ /**
+  * @apiDefine TokenError
+  * @apiError Unauthorized Invalid JWT token
+  *
+  * @apiErrorExample {json} Unauthorized-Error:
+  *     HTTP/1.1 401 Unauthorized
+  *     {
+  *       "status": 401,
+  *       "error": "Unauthorized"
+  *     }
+  */
 
+ /**
+  * @api {post} /v1/auth Authenticate user
+  * @apiVersion 1.0.0
+  * @apiName AuthUser
+  * @apiGroup Auth
+  *
+  * @apiParam {String} email  User email.
+  * @apiParam {String} password  User password.
+  *
+  * @apiExample Example usage:
+  * curl -H "Content-Type: application/json" -X POST -d '{ "email": "johndoe@gmail.com", "password": "foo", 'firstName': "John", "lastName": "Doe" }' localhost:5000/v1/auth
+  *
+  * @apiSuccess {Object}   user           User object
+  * @apiSuccess {ObjectId} user._id       User id
+  * @apiSuccess {String}   user.firstName      User first name
+  * @apiSuccess {String}   user.lastName      User last name
+  * @apiSuccess {String}   user.email  User email
+  * @apiSuccess {String}   token          Encoded JWT
+  *
+  * @apiSuccessExample {json} Success-Response:
+  *     HTTP/1.1 200 OK
+  *     {
+  *       "user": {
+  *          "_id": "56bd1da600a526986cf65c80"
+  *          "email": "johndoe@gmail.com"
+  *          "firstName": "John"
+  *          "lastName": "Doe"
+  *        },
+  *       "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWV9.TJVA95OrM7E2cBab30RMHrHDcEfxjoYZgeFONFh7HgQ"
+  *     }
+  *
+  * @apiError Unauthorized Incorrect credentials
+  *
+  * @apiErrorExample {json} Error-Response:
+  *     HTTP/1.1 401 Unauthorized
+  *     {
+  *       "status": 401,
+  *       "error": "Unauthorized"
+  *     }
+  */
 const authUser = async function (ctx, next) {
     return passport.authenticate('local', (user, err) => {
         if (!user) {
